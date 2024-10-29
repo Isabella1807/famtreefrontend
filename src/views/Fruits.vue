bbc8494182481f9eda1444e0b8743ff7917225b8
Isabella1807 <megaoreomuffin@hotmail.com>
2024-10-29 21:29:01 +0100

    conected

--- src/views/Fruits.vue
+++ src/views/Fruits.vue
@@ -1,32 +1,69 @@
 <script setup lang="ts">
-
 import Fruit from "@/components/Fruit.vue";
+import {useFruitStore} from "@/stores/fruit";
+import {ref} from "vue";
+
+const fruitStore = useFruitStore();
+
+console.log(fruitStore.list);
+
+const newFruit = ref('');
+
+const addNewFruit = () => {
+  if (newFruit.value.trim() === '') {
+    newFruit.value='';
+    return;
+  }
+  fruitStore.add(newFruit.value);
+  newFruit.value='';
+}
+
 </script>
 
 <template>
-  <div id="fruitinputContainer">
-    <form action="" id="formContainer">
-      <label for="fruitInput">Skriv en frugt, spade :)</label>
-      <input name="fruitInput" type="text" id="fruitInputArea">
-      <button type="submit" id="submitInputButton">Indsend frugt hihi</button>
-    </form>
-  </div>
-  <div>
-    <Fruit name="æble"/>
+  <div class="container">
+    <div>
+
+      <div id="fruitinputContainer">
+
+        <form id="formContainer" @submit.prevent>
+          <label for="fruitInput">Skriv en frugt hihi</label>
+          <div class="inputContainer">
+            <input name="fruitInput" type="text" class="fruitInputArea" v-model="newFruit">
+            <button type="submit" id="submitInputButton" @click="addNewFruit">Tilføj frugt hihi</button>
+          </div>
+
+        </form>
+
+      </div>
+
+      <div>
+        <Fruit v-for="item in fruitStore.list" :key="item.id" :name="item.name" :id="item.id"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-#formContainer{
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#formContainer {
   display: flex;
   flex-direction: column;
 }
 
-#fruitInputArea{
-  width: 300px;
+.inputContainer {
 }
 
-#submitInputButton{
+.fruitInputArea {
+  width: 200px;
+}
+
+#submitInputButton {
   width: 100px;
 }
 </style>
