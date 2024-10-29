14f556f1984f2c0539b5a5a74e4050ea664b37fe
Isabella1807 <megaoreomuffin@hotmail.com>
2024-10-29 20:18:14 +0100

    fruits

---
+++ src/components/Fruit.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+}>()
+
+// const props = defineProps(['name', 'title'])
+</script>
+
+<template>
+
+  <div>
+    <h2>{{props.name}}</h2>
+    <button>Slet mig</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Fruits.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import Fruit from "@/components/Fruit.vue";
+</script>
+
+<template>
+  <div id="fruitinputContainer">
+    <form action="" id="formContainer">
+      <label for="fruitInput">Skriv en frugt, spade :)</label>
+      <input name="fruitInput" type="text" id="fruitInputArea">
+      <button type="submit" id="submitInputButton">Indsend frugt hihi</button>
+    </form>
+  </div>
+  <div>
+    <Fruit name="æble"/>
+  </div>
+</template>
+
+<style scoped>
+#formContainer{
+  display: flex;
+  flex-direction: column;
+}
+
+#fruitInputArea{
+  width: 300px;
+}
+
+#submitInputButton{
+  width: 100px;
+}
+</style>
